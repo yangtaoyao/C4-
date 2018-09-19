4ac593263a35fc3f3cf9aaedef37b8af383bf120
yangtaoyao <1654366492@qq.com>
2018-09-19 12:28:15 +0800

    9-19

--- server/src/jieyou/util/RandomUtil.java
+++
@@ -1,7 +0,0 @@
-package jieyou.util;
-
-public class RandomUtil {
-	public static int randomRangeOf(int start , int end) {
-		return (int) (start+end*Math.random());
-	}
-}
--- server/src/jieyou/util/func/Handler.java
+++
@@ -1,7 +0,0 @@
-package jieyou.util.func;
-
-import org.apache.commons.fileupload.FileItem;
-
-public interface Handler {
-	public void handle(FileItem item,int i);
-}
